75af30ae0d39109b408ffb8d7d84b117d56d240f
isseium <issei@ikmz.net>
2014-09-03 21:34:50 +0900

    10000ストックまでの予想追加

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,6 +3,7 @@ class IndexController < ApplicationController
     @u = User.joins(:articles).group("users.id").order("SUM(stock_count) desc")
     @total_articles = Article.all.count
     @total_stock = Article.sum(:stock_count)
+    @estimate = 10000 / (@total_stock / @total_articles)
   end
   
   def crawl
