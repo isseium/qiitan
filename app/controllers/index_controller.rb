fd40a210f3fc404ef4ea5354c358932a25196bfa
manager <investi0173gate@yahoo.co.jp>
2014-10-03 14:00:17 +0900

    戻した

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,7 @@ class IndexController < ApplicationController
     @u = User.joins(:articles).group("users.id").order("SUM(stock_count) desc")
     @total_articles = Article.all.count
     @total_stock = Article.sum(:stock_count)
-    # @estimate = (10000 - @total_stock) / (@total_stock / @total_articles)
+    @estimate = (10000 - @total_stock) / (@total_stock / @total_articles)
     @estimate = 0
   end
   
